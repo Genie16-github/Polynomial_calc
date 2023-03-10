191cf30dfeba9aaa08776c1d801645a19f82f7e7
Genie16-github <4411dlwoqja@naver.com>
2023-03-10 15:43:28 +0900

    두 숫자 더하기

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -5,3 +5,16 @@ public class Main {
 
     }
 }
+
+class Calc{
+
+    public static int run(String exp) {
+
+        String[] bits = exp.replace(" ", "").split("\\+");
+
+        int a = Integer.parseInt(bits[0]);
+        int b = Integer.parseInt(bits[1]);
+
+        return a + b;
+    }
+}
